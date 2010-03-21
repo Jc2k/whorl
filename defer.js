cdbd460dba3bd5539e731b3eb78149af10ae7292
John Carr <john.carr@unrouted.co.uk>
2010-03-21 13:53:58 +0000

    Stub things out some more...

--- defer.js
+++ defer.js
@@ -1,26 +1,81 @@
 
-function success (value) {
-    // return a defer that has completed...
-    return value;
+
+var Deferred = function () {}
+Deferred.prototype = {
+    addCallbacks: function (callback, args, errback, args) {
+    },
+
+    addCallback: function (callable, args) {
+    },
+
+    addErrback: function (callable, args) {
+    },
+
+    addBoth: function (callable, args) {
+        // Add the same callable as a callback and an errback
+    },
+
+    callback: function (result) {
+    },
+
+    errback: function (result) {
+    },
+}
+
+function fail (value) {
+    var d = new Deferred ();
+    d.errback (value);
+    return d;
+}
+
+function succeed (value) {
+    var d = new Deferred ();
+    d.callback (value);
+    return d;
+}
+
+function maybeDeferred (value) {
+    if (isinstance (value, Deferred))
+        return value;
+    else
+        return succeed (value);
 }
 
 function async (fn) {
-    return function () {
-        var g = fn ();
+    var process = function (result, g, deferred) {
         try {
+            result = g.next ();
             while (1) {
-                var captured = g.next ();
+                if (isinstance (result, Deferred)) {
+                    result.addBoth (process, g, deferred);
+                    return deferred;
+                } else if (isinstance (result, Failure)) {
+                    g.throw (result);
+                } else {
+                    result = g.send (result);
+                }
             }
-        } catch (StopIteration) {
-            return success ();
+        } catch (e if e == "ReturnValue") {
+            deferred.callback (); // FIXME: Need to capture return value
+            return deferred;
+        } catch (e if e =="StopIteration") {
+            deferred.callback ();
+            return deferred;
+        } catch (e) {
+            deferred.errback (e);
+            return deferred;
         }
+    }
+
+    return function () {
+        return process (null, fn (), new Deferred ());
     }
 }
 
 var badger = async (function () {
-    log ("entering badger function and yielding");
-    yield;
-    log("continuing badger function");
+    print ("entering badger function and yielding");
+    var a = yield succeed (20);
+    print ("continuing badger function");
 });
 
 badger ();
--- runner.sh
+++ runner.sh
@@ -1,1 +1,5 @@
-GJS_DEBUG_TOPICS="JS LOG" GJS_DEBUG_OUTPUT=stderr LD_LIBRARY_PATH=/usr/lib/xulrunner-1.9.1.8/ gjs defer.js
+if [ -f /usr/bin/smjs ] ; then
+    smjs -v 170 defer.js
+else
+    GJS_DEBUG_TOPICS="JS LOG" GJS_DEBUG_OUTPUT=stderr LD_LIBRARY_PATH=/usr/lib/xulrunner-1.9.1.8/ gjs defer.js
+fi
